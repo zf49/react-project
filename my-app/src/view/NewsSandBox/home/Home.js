4ea659c53960a6f2d9eae9038a9df0c0ee2514da
zf49 <zwan686@aucklanduni.ac.nz>
2022-12-01 23:09:19 +1300

    json server get and post

--- my-app/src/view/NewsSandBox/home/Home.js
+++ my-app/src/view/NewsSandBox/home/Home.js
@@ -2,14 +2,41 @@ import React from 'react'
 import { SearchOutlined } from '@ant-design/icons';
 import { Button, Tooltip, Space } from 'antd';
 
+import axios from 'axios'
 
-import { Layout, Menu } from 'antd';
-const { Header, Sider, Content } = Layout;
+
 
 export default function Home() {
+
+
+    let testJsonServer = ()=>{
+            // get data
+            // axios.get("http://localhost:8000/posts").then(res=>{console.log(res.data)})
+
+            // post data
+
+            axios.post("http://localhost:8000/posts", {  "title": "3333", 
+            "author": "AAAA" }
+            ).then(res=>{
+                console.log(res.data)
+            })
+           
+
+
+    }
+
+
     return (
-       <div>
-           Home
-       </div>
+        <div>
+            Home
+           
+            
+            <Button type="primary" icon={<SearchOutlined />} onClick={()=>{
+                testJsonServer()    
+            }}>
+                        Search
+            </Button>
+
+        </div>
     )
 }
