97704946863dd94d3477502303dc232a37a53060
zf49 <zwan686@aucklanduni.ac.nz>
2022-12-01 23:24:32 +1300

    json server advanced

--- my-app/src/view/NewsSandBox/home/Home.js
+++ my-app/src/view/NewsSandBox/home/Home.js
@@ -13,14 +13,44 @@ export default function Home() {
             // get data
             // axios.get("http://localhost:8000/posts").then(res=>{console.log(res.data)})
 
-            // post data
 
-            axios.post("http://localhost:8000/posts", {  "title": "3333", 
-            "author": "AAAA" }
-            ).then(res=>{
-                console.log(res.data)
-            })
-           
+            // post data
+            // axios.post("http://localhost:8000/posts", {  "title": "3333", 
+            // "author": "AAAA" }
+            // ).then(res=>{
+            //     console.log(res.data)
+            // })
+
+
+
+            // update data
+            //replacement update
+            // axios.put("http://localhost:8000/posts/1",{
+            //     title:"11111-update"
+            // }).then(res=>{console.log(res.data)})
+
+            // patch update
+            // axios.patch("http://localhost:8000/posts/1",{
+            //     title:"111111-patch update"
+            // }).then(res=>{console.log(res.data)})
+
+
+            // delete
+            // axios.delete("http://localhost:8000/posts/1").then(res=>{console.log(res.data)})
+
+
+
+            // advanced opration _embed
+            // get down data
+
+             //    axios.get("http://localhost:8000/posts?_embed=comments").then(res=>{console.log(res.data)})
+
+
+            // advanced opration _expand
+            // get up data    
+        
+            axios.get("http://localhost:8000/comments?_expand=post").then(res=>{console.log(res.data)})
+
 
 
     }
