f0b877bbe8bc3d69d70403629b4c0c7e7fa0118a
zf49 <zwan686@aucklanduni.ac.nz>
2022-12-02 00:49:39 +1300

    side bar

--- my-app/src/component/sandbox/SideBar.js
+++ my-app/src/component/sandbox/SideBar.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
+import axios from'axios'
+import ReactHtmlParser from 'html-react-parser';
 import { useNavigate } from 'react-router-dom'
 import { Layout, Menu } from 'antd';
 import {
-    AppstoreOutlined, MailOutlined, SettingOutlined
+    AppstoreOutlined, MailOutlined, SettingOutlined,
+    HomeOutlined
   } from '@ant-design/icons';
 
 const {  Sider } = Layout;
 
+export default function SideBar() {
 
-function getItem(label, key, icon, children, type) {
+
+  function getItem(label, key, icon, children, type) {
     return {
       key,
       icon,
@@ -19,29 +24,50 @@ function getItem(label, key, icon, children, type) {
     };
   }
 
-
-  // fake data!!!, for real, using axios interact with back-end, get the data like above stracture.
-  // iterate the json, like below.
-  const items = [
-    getItem('Home', '/home', <MailOutlined />),
-    getItem('User Management', '/user-manage', <MailOutlined />, [
-      getItem('User List', '/user-manage/list')
-    ]),
-    getItem('Authority Management', '/right-manage', <AppstoreOutlined />, [
-      getItem('Role List', '/right-manage/role/list'),
-      getItem('Authority List', '/right-manage/auth/list'),
-      
-    ])
-  ];
-
-
-
-
-
-export default function SideBar() {
-
     const [collapsed, setCollapsed] = useState(false);
     const navigate = useNavigate()
+
+    const [menu, setMenu] = useState([])
+    const items = [];
+    const itemChildren = undefined
+
+    const checkPermission = (children)=>{
+
+      const newChild = []
+      children.map((item)=>{
+        
+          if (item.pagepermisson === 1){
+            newChild.push(item)
+          }
+      })
+      return newChild
+    }
+
+    useEffect(() => {
+      
+      axios.get("http://localhost:8000/rights?_embed=children").then(res=>{
+        console.log(res.data)
+          
+        res.data.map((item)=>{
+
+              if(item.children.length===0){
+                items.push(getItem(item.label,item.key,null,undefined))
+              }else{
+                items.push(getItem(item.label,item.key,item.icon,checkPermission(item.children)))
+              }
+            
+
+          
+        })
+        setMenu(items)
+        // console.log(menu)
+
+      })
+
+    }, [])
+
+      
+    
 
 
     const onClick = (e) => {
@@ -76,13 +102,12 @@ export default function SideBar() {
 
     return (
         <Sider trigger={null} collapsible collapsed={false} width={300} >
-            
         <div className="logo">Golbal News Admin System</div>
         <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={['1']}
-          items={items}
+          items={menu}
           onClick={onClick}
 
         //   defaultOpenKeys={['sub1']}
