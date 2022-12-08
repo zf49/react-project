28757c0ed16ebb4e772eb1166739f5e8f6942a00
zf49 <zwan686@aucklanduni.ac.nz>
2022-12-08 18:33:36 +1300

    button

--- my-app/src/view/NewsSandBox/auth-manage/AuthList.js
+++ my-app/src/view/NewsSandBox/auth-manage/AuthList.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from'axios'
 
+
+import { Table,Tag,Button } from 'antd';
+import{ DeleteOutlined,EditOutlined} from '@ant-design/icons'
 export default function AuthList() {
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/rights").then(res=>{
+            setDataSource(res.data)
+        })
+    }, [])
+
+
+
+    const [dataSource, setDataSource] = useState([])
+
+      
+      const columns = [
+        {
+          title: 'ID',
+          dataIndex: 'id',
+          render:(id)=>{
+            return  <b>{id}</b>
+        }
+        },
+        {
+          title: 'TITLE',
+          dataIndex: 'label',
+        },
+        {
+          title: 'URL',
+          dataIndex: 'key',
+          render:(key)=>{
+              return <Tag color="green">{key}</Tag>
+          }
+        },
+        {
+            title:"OPERTATION",
+            render:()=>{
+                return <div>
+                    <Button danger shape="circle" icon={<DeleteOutlined />}></Button>
+                    <Button type="primary" shape="circle" icon={<EditOutlined />}></Button>
+                    
+                </div>
+            }
+        }
+      ];
+      
     return (
         <div>
-            RightList
+        
+        
+        <Table dataSource={dataSource} columns={columns}/>;
+
+
         </div>
     )
 }
